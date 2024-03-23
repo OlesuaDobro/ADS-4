963d0e1f40f4ac146e4db6b2ab5d2ceceb2a3b47
Dobro <76173176+OlesuaDobro@users.noreply.github.com>
2024-03-23 19:24:08 +0300

    6

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,49 +1,47 @@
 // Copyright 2021 NNTU-CS
-int countpairs1(int *arr, int len, int value) {
+int countpairs1(int *arr, int len, int value){
     int count = 0;
-    for (int i = 0; i < len; i++) {
-        for (int j = i + 1; j < len; j++) {
-            if (arr[i] + arr[j] == value) {
+    for (int i = 0; i < len - 1; i++){
+        for (int j = i + 1; j < len; j++){
+            if (arr[i] + arr[j] == value)
                 count++;
-            }
         }
     }
     return 0;
 }
-int countpairs2(int *arr, int len, int value) {
+int countpairs2(int *arr, int len, int value){
     int count = 0;
     int left = 0;
     int right = len - 1;
-    while (left < right) {
+    while (left < right){
         int sum = arr[left] + arr[right];
-        if (sum == value) {
+        if (sum == value){
             count++;
             left++;
             right--;
-        } else if (sum < value) {
+        } else if (sum < value)
             left++;
-        } else {
+        else
             right--;
-        }
     }
     return 0;
 }
-int countPairs3(int *arr, int len, int value) {
-  int count = 0;
-    for (int i = 0; i < len; i++) {
-        int target = value - arr[i];
-        int left = i + 1;
-        int right = len - 1;
-        while (left <= right) {
-            int mid = left + (right - left) / 2;
-            if (arr[mid] == target) {
+int countpairs3(int *arr, int len, int value){
+    int count = 0;
+    for (int i = 0; i < len - 1; i++){
+        int low = i + 1;
+        int high = len - 1;
+        while (low <= high){
+            int mid = (low + high) / 2;
+            int sum = arr[i] + arr[mid];
+            if (sum == value){
                 count++;
                 break;
-            } else if (arr[mid] < target) {
-                left = mid + 1;
-            } else {
-                right = mid - 1;
             }
+            else if (sum < value)
+                low = mid + 1;
+            else
+                high = mid - 1;
         }
     }
     return 0;
