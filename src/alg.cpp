53af8609dc33ed243b07ca37cfb2317b6ff6e555
Dobro <76173176+OlesuaDobro@users.noreply.github.com>
2024-03-23 19:31:59 +0300

    8

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,7 +1,7 @@
 // Copyright 2021 NNTU-CS
 int countpairs1(int *arr, int len, int value) {
     int count = 0;
-    for (int i = 0; i < len - 1; i++){
+    for (int i = 0; i < len - 1; i++) {
         for (int j = i + 1; j < len; j++) {
             if (arr[i] + arr[j] == value)
                 count++;
@@ -13,9 +13,9 @@ int countpairs2(int *arr, int len, int value) {
     int count = 0;
     int left = 0;
     int right = len - 1;
-    while (left < right){
+    while (left < right) {
         int sum = arr[left] + arr[right];
-        if (sum == value){
+        if (sum == value) {
             count++;
             left++;
             right--;
@@ -31,14 +31,13 @@ int countpairs3(int *arr, int len, int value) {
     for (int i = 0; i < len - 1; i++) {
         int low = i + 1;
         int high = len - 1;
-        while (low <= high){
+        while (low <= high) {
             int mid = (low + high) / 2;
             int sum = arr[i] + arr[mid];
-            if (sum == value){
+            if (sum == value) {
                 count++;
                 break;
-            }
-            else if (sum < value)
+            } else if (sum < value)
                 low = mid + 1;
             else
                 high = mid - 1;
