702ba0985d89f20a4e1ef8b6a92f76c7f764c9b7
Dobro <76173176+OlesuaDobro@users.noreply.github.com>
2024-03-23 21:31:52 +0300

    15

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,5 +1,5 @@
 // Copyright 2021 NNTU-CS
-int countPairs(int *arr, int len, int value) {
+int countPairs1(int *arr, int len, int value) {
     int count = 0;
     for (int i = 0; i < len - 1; i++) {
         for (int j = i + 1; j < len; j++) {
@@ -10,7 +10,7 @@ int countPairs(int *arr, int len, int value) {
     }
     return 0;
 }
-int countPairs(int *arr, int len, int value) {
+int countPairs2(int *arr, int len, int value) {
     int count = 0;
     int left = 0;
     int right = len - 1;
@@ -28,7 +28,7 @@ int countPairs(int *arr, int len, int value) {
     }
     return 0;
 }
-int countPairs(int *arr, int len, int value) {
+int countPairs3(int *arr, int len, int value) {
     int count = 0;
     for (int i = 0; i < len - 1; i++) {
         int low = i + 1;
