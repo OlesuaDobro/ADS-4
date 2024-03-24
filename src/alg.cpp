60cd0259d097496db4c2cbd547caf38aff14b5ef
Dobro <76173176+OlesuaDobro@users.noreply.github.com>
2024-03-24 11:51:41 +0300

    16

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,7 +1,7 @@
 // Copyright 2021 NNTU-CS
-int countPairs1(int *arr, int len, int value) {
+int countpairs1(int *arr, int len, int value) {
     int count = 0;
-    for (int i = 0; i < len - 1; i++) {
+    for (int i = 0; i < len; i++) {
         for (int j = i + 1; j < len; j++) {
             if (arr[i] + arr[j] == value) {
                 count++;
@@ -10,7 +10,7 @@ int countPairs1(int *arr, int len, int value) {
     }
     return 0;
 }
-int countPairs2(int *arr, int len, int value) {
+int countpairs2(int *arr, int len, int value) {
     int count = 0;
     int left = 0;
     int right = len - 1;
@@ -20,7 +20,8 @@ int countPairs2(int *arr, int len, int value) {
             count++;
             left++;
             right--;
-        } else if (sum < value) {
+        }
+        else if (sum < value) {
             left++;
         } else {
             right--;
@@ -28,18 +29,18 @@ int countPairs2(int *arr, int len, int value) {
     }
     return 0;
 }
-int countPairs3(int *arr, int len, int value) {
+int countpairs3(int *arr, int len, int value) {
     int count = 0;
-    for (int i = 0; i < len - 1; i++) {
+    for (int i = 0; i < len; i++) {
+        int target = value - arr[i];
         int low = i + 1;
         int high = len - 1;
         while (low <= high) {
             int mid = (low + high) / 2;
-            int sum = arr[i] + arr[mid];
-            if (sum == value) {
+            if (arr[mid] == target) {
                 count++;
                 break;
-            } else if (sum < value) {
+            } else if (arr[mid] < target) {
                 low = mid + 1;
             } else {
                 high = mid - 1;
