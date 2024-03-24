78dcddf75e6ae4ccad45145f5d22e3bb1ce916c8
Dobro <76173176+OlesuaDobro@users.noreply.github.com>
2024-03-24 12:36:41 +0300

    22

--- src/alg.cpp
+++ src/alg.cpp
@@ -26,28 +26,24 @@ int countPairs2(int *arr, int len, int value) {
             right--;
         }
     }
-    return count;
-}
-int binarySearch(int *arr, int start, int end, int target) {
-    while (start <= end) {
-        int mid = (start + end) / 2;
-        if (arr[mid] == target) {
-            return mid;
-        } else if (arr[mid] < target) {
-            start = mid + 1;
-        } else {
-            end = mid - 1;
-        }
-    }
-    return -1;
+    return 0;
 }
 int countPairs3(int *arr, int len, int value) {
     int count = 0;
     for (int i = 0; i < len; i++) {
-        int diff = value - arr[i];
-        int index = binarySearch(arr, i + 1, len - 1, diff);
-        if (index != -1) {
-            count++;
+        int target = value - arr[i];
+        int low = i + 1;
+        int high = len - 1;
+        while (low <= high) {
+            int mid = (low + high) / 2;
+            if (arr[mid] == target) {
+                count++;
+                break;
+            } else if (arr[mid] < target) {
+                low = mid + 1;
+            } else {
+                high = mid - 1;
+            }
         }
     }
     return 0;
