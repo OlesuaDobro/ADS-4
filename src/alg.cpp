adae1dc865f4ceea031965b288891dc7c4ba32a5
Dobro <76173176+OlesuaDobro@users.noreply.github.com>
2024-03-24 13:24:46 +0300

    26

--- src/alg.cpp
+++ src/alg.cpp
@@ -29,22 +29,22 @@ int countPairs2(int *arr, int len, int value) {
     return count;
 }
 int countPairs3(int* arr, int len, int value) {
-    int count = 0, j = 0, left = 0, right = len;
+    int count = 0, y = 0, left = 0, right = len;
     for (int i = 0; i < (len - 1); i++) {
         if (arr[i] > value) {
             break;
         }
         left = i;
-        right = len--;
+        right = len - 1;
         while (left < right) {
-            j = left + ((right - left) / 2);
+            y = left + ((right - left) / 2);
             if (left == right) {
                 break;
             }
-            if ((arr[i] + arr[j]) > value) {
-                for (int n = (j - 1); n >= left; n--) {
-                    right = n++;
-                    if (arr[j] != arr[n]) {
+            if ((arr[i] + arr[y]) > value) {
+                for (int n = (y - 1); n >= left; n--) {
+                    right = n + 1;
+                    if (arr[y] != arr[n]) {
                         break;
                     }
                 }
@@ -52,10 +52,10 @@ int countPairs3(int* arr, int len, int value) {
                     break;
                 }
             }
-            if ((arr[i] + arr[j]) < value) {
-                for (int n = (j + 1); n <= right; n++) {
-                    left = n--;
-                    if (arr[j] != arr[n]) {
+            if ((arr[i] + arr[y]) < value) {
+                for (int n = (y + 1); n <= right; n++) {
+                    left = n - 1;
+                    if (arr[y] != arr[n]) {
                         break;
                     }
                 }
@@ -63,26 +63,26 @@ int countPairs3(int* arr, int len, int value) {
                     break;
                 }
             }
-            if ((arr[i] + arr[j]) == value) {
+            if ((arr[i] + arr[y]) == value) {
                 int t = right;
-                for (int n = (j + 1); n <= t; n++) {
+                for (int n = (y + 1); n <= t; n++) {
                     right = n - 1;
-                    if (arr[j] != arr[n]) {
+                    if (arr[y] != arr[n]) {
                         break;
                     }
                 }
                 t = left;
-                for (int n = (j - 1); n >= t; n--) {
-                    left = n++;
-                    if (arr[j] != arr[n]) {
+                for (int n = (y - 1); n >= t; n--) {
+                    left = n + 1;
+                    if (arr[y] != arr[n]) {
                         break;
                     }
                 }
                 break;
             }
         }
-        for (int y = left; y <= right; y++) {
-            if (((arr[i] + arr[y]) == value) && (i != y)) {
+        for (int j = left; j <= right; j++) {
+            if (((arr[i] + arr[j]) == value) && (i != j)) {
                 count++;
             }
         }
