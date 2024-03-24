46848dc3789e0698c9806c4851361a2450283aa1
Dobro <76173176+OlesuaDobro@users.noreply.github.com>
2024-03-24 13:38:16 +0300

    30

--- src/alg.cpp
+++ src/alg.cpp
@@ -11,94 +11,43 @@ int countPairs1(int *arr, int len, int value) {
             }
         }
     }
-    return count;
+    return 0;
 }
-int countPairs2(int* arr, int len, int value) {
-    int count = 0, x = 0, j = 0;
-    for (int i = 0; i < (len - 1); i++) {
-        if ((arr[i] + arr[i + 1]) >= value) {
-            x = i;
-            break;
-        }
-    }
-    for (int i = 0; i < (len - 1); i++) {
-        if (arr[i] > value) {
-            break;
-        }
-        if (x <= i) {
-            j = i + 1;
+int countPairs2(int *arr, int len, int value) {
+    int count = 0;
+    int left = 0;
+    int right = len - 1;
+    while (left < right) {
+        int sum = arr[left] + arr[right];
+        if (sum == value) {
+            count++;
+            left++;
+            right--;
+        } else if (sum < value) {
+            left++;
         } else {
-            j = x;
-        }
-        for (; j < len; j++) {
-            if ((arr[i] + arr[j]) == value) {
-                count++;
-            }
-            if (arr[j] > value) {
-                break;
-            }
+            right--;
         }
     }
-    return count;
+    return 0;
 }
-int countPairs3(int* arr, int len, int value) {
-    int count = 0, y = 0, left = 0, right = len;
-    for (int i = 0; i < (len - 1); i++) {
-        if (arr[i] > value) {
-            break;
-        }
-        left = i;
-        right = len - 1;
-        while (left < right) {
-            y = left + ((right - left) / 2);
-            if (left == right) {
-                break;
-            }
-            if ((arr[i] + arr[y]) > value) {
-                for (int n = (y - 1); n >= left; n--) {
-                    right = n + 1;
-                    if (arr[y] != arr[n]) {
-                        break;
-                    }
-                }
-                if (right - left == 1) {
-                    break;
-                }
-            }
-            if ((arr[i] + arr[y]) < value) {
-                for (int n = (y + 1); n <= right; n++) {
-                    left = n - 1;
-                    if (arr[y] != arr[n]) {
-                        break;
-                    }
-                }
-                if (right - left == 1) {
-                    break;
-                }
-            }
-            if ((arr[i] + arr[y]) == value) {
-                int t = right;
-                for (int n = (y + 1); n <= t; n++) {
-                    right = n - 1;
-                    if (arr[y] != arr[n]) {
-                        break;
-                    }
-                }
-                t = left;
-                for (int n = (y - 1); n >= t; n--) {
-                    left = n + 1;
-                    if (arr[y] != arr[n]) {
-                        break;
-                    }
-                }
-                break;
-            }
-        }
-        for (int j = left; j <= right; j++) {
-            if (((arr[i] + arr[j]) == value) && (i != j)) {
+int countPairs3(int *arr, int len, int value) {
+    int count = 0;
+    for (int i = 0; i < len; i++) {
+        int target = value - arr[i];
+        int low = i + 1;
+        int high = len - 1;
+        while (low <= high) {
+            int mid = (low + high) / 2;
+            if (arr[mid] == target) {
                 count++;
+                break;
+            } else if (arr[mid] < target) {
+                low = mid + 1;
+            } else {
+                high = mid - 1;
             }
         }
     }
-    return count;
+    return 0;
 }
